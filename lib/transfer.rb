f2a861cae917f32d398145d211ce0110628e6ddf
remote-torrent-6747 <abbyc@sky.com>
2021-01-24 14:26:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,6 +34,7 @@ class Transfer
     if self.status == "complete"
       @sender.balance = @sender.balance + @amount
       @receiver.balance = @receiver.balance - @amount
+      @status = "reversed"
     end
   end
 
