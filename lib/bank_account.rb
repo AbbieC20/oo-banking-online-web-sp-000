f8c4ad90d29e2ed5e8a00fa12abbcd1530912be7
remote-torrent-6747 <abbyc@sky.com>
2021-01-24 13:09:03 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+  attr_accessor :name, :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
 
 end
+
+# initialize
+# a Bank Account
+# a name Y
+# balance of 1000 Y
+# status of 'open'Y
+# end
+
+# deposit
+# can deposit money into account
+# end
+
+# display balance
+# show balance
+# end
+
+# valid?
+# is true when status = open and balance > 0
+# end
+
+# close Account
+# sets status to closed.
+# end
