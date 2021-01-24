b9b391de9ce9ea5c6f00098fb08594d1f8ca173d
remote-torrent-6747 <abbyc@sky.com>
2021-01-24 14:06:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,8 @@ class Transfer
 
   def execute_transaction
     if self.valid? == true && self.status != "complete"
-      @sender.balance = @sender.balance - figure
-      @receiver.balance = @receiver.balance + figure
+      @sender.balance = @sender.balance - @amount
+      @receiver.balance = @receiver.balance + @amount
       @status = "complete"
     else
       @status = "rejected"
